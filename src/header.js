b4bdb329bf0d2415c4c3fc997a6b28f9101ea46c
Connor Yanz <connor.yanz@gmail.com>
2018-10-08 00:51:48 -0400

    First build

--- src/header.js
+++ src/header.js
@@ -1,10 +1,11 @@
 
 import React, { Component } from 'react';
+import './header.scss';
 
 class Header extends Component {
   render() {
     return (
-      <div className="header">This is the header
+      <div className="header-title">This is the header
       </div>
     );
   }
