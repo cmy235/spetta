5c9a2799cb8c02da1e7e64aaff9bdd456fcf13b5
Connor Yanz <connor.yanz@gmail.com>
2018-10-08 00:22:16 -0400

    Add header and welcome components

---
+++ src/header.js
@@ -0,0 +1,13 @@
+
+import React, { Component } from 'react';
+
+class Header extends Component {
+  render() {
+    return (
+      <div className="header">This is the header
+      </div>
+    );
+  }
+}
+
+export default Header;
